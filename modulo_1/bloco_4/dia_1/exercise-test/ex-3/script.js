c47f6f9b7985ddff800aa5646dcaa9dbbc73522f
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-19 18:31:43 -0300

    Começa exercícios bloco 4 dia 1

---
+++ modulo_1/bloco_4/dia_1/exercise-test/ex-3/script.js
@@ -0,0 +1,11 @@
+let notaPessoa = 90;
+
+if (notaPessoa >= 80) {
+  console.log("Parabéns, você foi aprovado");
+}
+else if (notaPessoa < 80 && notaPessoa >= 60) {
+  console.log("Você está na nossa lista de espera");
+}
+else {
+  console.log("Você foi reprovado");
+}
