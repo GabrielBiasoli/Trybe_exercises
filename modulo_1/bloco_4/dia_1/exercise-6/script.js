5e21488af7ee77e400970960e40dd83a977cc6f0
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-19 21:40:25 -0300

    Finaliza exercicio 6

---
+++ modulo_1/bloco_4/dia_1/exercise-6/script.js
@@ -0,0 +1,33 @@
+let x = "pawn";
+
+x = x.toLowerCase();
+
+switch (x) {
+  case "king":
+    console.log("one square in any direction");
+    break;
+
+  case "queen":
+    console.log("diagonally, horizontally, or vertically any number of squares");
+    break;
+
+  case "rook":
+    console.log("horizontally or vertically any number of squares");
+    break;
+
+  case "bishop":
+    console.log("diagonally any number of squares");
+    break;
+
+  case "knight":
+    console.log("in an ‘L’ shape’: two squares in a horizontal or vertical direction");
+    break;
+
+  case "pawn":
+    console.log("vertically forward one square, with the option to move two squares if they have not yet moved");
+    break;
+
+  default:
+    console.log("Error: Piece not found");
+    break;
+}
