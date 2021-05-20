11c7c6f00d90f5e4bb90eb58fa07aef8dc7a9eee
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-20 18:45:21 -0300

    Finalizado exercicio 5 - estrutura For com Array

---
+++ modulo_1/bloco_4/dia_2/ex-5.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let x = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (x < numbers[index]) {
+    x = numbers[index]
+  }
+}
+
+console.log("Maior valor:", x);
