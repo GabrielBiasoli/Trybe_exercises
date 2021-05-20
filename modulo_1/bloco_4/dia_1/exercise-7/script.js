6b2c80ba5eb6a01f5b62de0b1ce11bbfd43b9273
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-19 21:52:31 -0300

    Inicia exercicio 7

---
+++ modulo_1/bloco_4/dia_1/exercise-7/script.js
@@ -0,0 +1,23 @@
+let x = 20;
+
+if (x >= 90) {
+  console.log("A");
+
+} else if (x >= 80) {
+  console.log("B");
+
+} else if (x >= 70) {
+  console.log("C");
+
+} else if (x >= 60) {
+  console.log("D");
+
+} else if (x >= 50) {
+  console.log("E");
+} else {
+
+} {
+
+} {
+
+}
