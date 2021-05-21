5613969470a1d4d18ce5d3fcdb0c918484f5bd2a
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-21 17:59:59 -0300

    Resolve o primeiro exercicio

---
+++ modulo_1/bloco_4/dia_3/ex-1.js
@@ -0,0 +1,12 @@
+let n = 7;
+let asteriscos = "";
+
+for (let secondI = 0; secondI < n; secondI += 1) {
+  asteriscos += "*";
+
+}
+for (let i = 0; i < n; i += 1) {
+
+  console.log(asteriscos);
+};
+
