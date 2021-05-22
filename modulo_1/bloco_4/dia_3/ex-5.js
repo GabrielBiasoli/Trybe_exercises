373afcf6cd5fe66734b836ef55c35d7e4b41359f
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-05-22 16:59:52 -0300

    Finaliza exercicio 5

---
+++ modulo_1/bloco_4/dia_3/ex-5.js
@@ -0,0 +1,40 @@
+let n = 7;
+let x = "";
+
+
+let n2 = Math.ceil(n / 2);
+let p3 = n2 - 1;
+let p4 = n2 - 1;
+
+
+if (n % 2 != 0) {
+
+  for (let i2 = 0; i2 < n2; i2 += 1) {
+
+
+    for (let i = 0; i < n; i += 1) {
+
+
+      if ((i == p3) || (i == p4) || (i2 == n2 - 1)) {
+        x += "*";
+      } else {
+        x += " ";
+      }
+
+
+    }
+
+    console.log(x);
+    x = "";
+    p3 -= 1;
+    p4 += 1;
+  }
+
+
+
+
+
+}
+else {
+  console.log("Only odd numbers are allowed! Try again.");
+}
