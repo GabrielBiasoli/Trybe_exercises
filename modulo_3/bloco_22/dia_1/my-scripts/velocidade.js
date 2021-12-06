52bbc4c74217db5efd3c7e2756fe554cb360cdfe
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-12-06 17:56:37 -0300

    Finaliza exercicios do dia

--- modulo_3/bloco_22/dia_1/my-scripts/velocidade.js
+++
@@ -1,6 +0,0 @@
-const { questionInt, questionFloat } = require('readline-sync');
-
-const raceTime = questionInt('Qual foi a duração da corrida(seg)? ');
-const distance = questionFloat('Qual a distância percorrida(metros)?');
-
-console.log(`Velocidade média: ${(distance / raceTime).toFixed(2)} m/s`);
